0432653048d6fdaefda3a372d5b9e67c8ee2a80f
user <user@WIN-2MSGKLHNS9A>
2023-12-26 17:23:25 +0300

    Добавьте файлы проекта.

---
+++ Lab11/LinearEquation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab11
+{
+    struct LinearEquation
+    {
+        double k;
+        double b;
+        
+
+        public LinearEquation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;            
+        }
+
+        public string Root()
+        {
+            if (k == 0)
+            {
+                if (b == 0)
+                    return "Бесконечное количество решений";
+                else
+                    return "Решений нет";
+            }
+
+            double root = -b / k;
+            return $"Решение уравнения: x = {root}";
+        }
+    }
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            
+            LinearEquation linearEquation = new LinearEquation(k, b);
+            Console.WriteLine(linearEquation.Root());
+            Console.ReadKey();
+        }
+    }
+}
